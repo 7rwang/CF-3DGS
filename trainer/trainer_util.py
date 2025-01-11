cbe67123feff7459c6929ac9e8bca40a36e91238
王麒睿 <wwang@MBP.local>
2025-01-11 21:16:41 +0800

    Modify render to support 4 cameras

--- trainer/trainer_util.py
+++ trainer/trainer_util.py
@@ -696,8 +696,8 @@ class GaussianTrainer(object):
 
             # ------------------------------process 4-dim indices------------------------------、
             print("idx:", idx)
-            print("image_names:", image_names)
             image_names = [self.data[i] for i in idx]
+            print("image_names:", image_names)
             original_images = [Image.open(image_name).convert("RGB") for image_name in image_names]
             color_torch_list = []
             for original_image in original_images:
