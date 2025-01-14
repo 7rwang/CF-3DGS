45ccdbc3d1276e1313a359cfba3ad879b0953126
王麒睿 <wwang@MBP.local>
2025-01-14 11:52:41 +0800

    Modify render to support 4 cameras

--- scene/test.py
+++ scene/test.py
@@ -2,7 +2,7 @@
 
 import os
 print("Current Working Directory:", os.getcwd())
-from scene.dataset_readers import readColmapCameras
+# from scene.dataset_readers import readColmapCameras
 from scene.colmap_loader import read_extrinsics_binary,read_intrinsics_binary
 
 path = "/home/xduo/桌面/CF-3DGS/data/car_4v"
